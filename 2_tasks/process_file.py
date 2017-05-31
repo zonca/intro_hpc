ce7e5c20eae16ab248836f18b7e8d62bc7473256
Andrea Zonca <code@andreazonca.com>
2017-05-30 17:50:49 -0700

    turn into a module

--- 2_tasks/process_file.py
+++ 2_tasks/process_file.py
@@ -1,18 +1,22 @@
-import sys
 import time
 import numpy as np
 from pathlib import Path
 
-filename = Path(sys.argv[1])
-print("Processing {}".format(filename))
+def analyze(input_filename):
+    filename = Path(input_filename)
+    print("Processing {}".format(filename))
 
-output_filename = filename.parent / "output" / ("out_" + filename.name)
+    output_filename = filename.parent / "output" / ("out_" + filename.name)
 
-sleep_minutes = 1
-time.sleep(sleep_minutes * 60)
+    sleep_minutes = 1
+    time.sleep(sleep_minutes * 60)
 
-print("Writing output file {}".format(output_filename))
+    print("Writing output file {}".format(output_filename))
 
-output_filename.parent.mkdir(exist_ok=True)
-output_file = open(output_filename, "w")
-output_file.close()
+    output_filename.parent.mkdir(exist_ok=True)
+    output_file = open(output_filename, "w")
+    output_file.close()
+
+if __name__ == "__main__":
+    import sys
+    analyze(sys.argv[1])
